a2d628478ba32a68954c50f331f0886efcf5d805
ODM <danmusad@gmail.com>
2020-10-03 08:52:59 -0500

    Initial Commit

---
+++ UIC_Job_Finder/UIC_Job_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIC_Job_FinderApp.swift
+//  UIC_Job_Finder
+//
+//  Created by ODM on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct UIC_Job_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
